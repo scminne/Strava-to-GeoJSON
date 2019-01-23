763c7648b1f319534d36f320f762a01c0bf870a2
remisalmon <salmon.remi@gmail.com>
2019-01-23 10:39:49 -0700

    Add power calculation

--- strava_geojson.py
+++ strava_geojson.py
@@ -14,14 +14,28 @@ import numpy as np
 import matplotlib.cm as cm
 
 from scipy.signal import medfilt
-from matplotlib.colors import to_hex
 
 # functions
+def rgb2hex(c):
+    hex = '#%02x%02x%02x'%(int(c[0]*255), int(c[1]*255), int(c[2]*255))
+    
+    return(hex)
+
 def main(): # main script
     # parameters
-    vis_colormap = 'jet'
-    vis_data = 'speed' # 'none', 'elevation', 'slope' or 'speed'
+    vis_data = 'speed' # 'none', 'elevation', 'slope', 'speed' or 'power'
     vis_medium = 'geojsonio' # 'raw', 'geojsonio' or 'umap'
+    rider_weight = 160*0.45359237 # kg
+    bike_weight = 32.6*0.45359237 # kg
+
+    # constants
+    vis_colormap = 'jet'
+    rider_bike_frontal_area = 0.632 # m^2 (from https://www.cyclingpowerlab.com/cyclingaerodynamics.aspx)
+    rider_bike_drag_coeff = 1.15 # unitless (from https://www.cyclingpowerlab.com/cyclingaerodynamics.aspx)
+    bike_drivetrain_loss = 7 # % (from https://en.wikipedia.org/wiki/Bicycle_performance#Mechanical_efficiency)
+    bike_rr_coeff = 0.006 # unitless (from https://en.wikipedia.org/wiki/Bicycle_performance#Rolling_resistance)
+    air_density = 1.225 # kg/m^3
+    g = 9.80665 # m/s^2
 
     # find and read GPX file
     gpx_file = glob.glob('*.gpx')[0]
@@ -45,14 +59,12 @@ def main(): # main script
                 lon = float(tmp[1])
 
                 lat_lon_data.append([lat, lon])
-
             elif '<ele' in line: # get trackpoints elevation
                 tmp = re.findall('\d+\.\d+', line)
 
                 elevation = float(tmp[0])
 
                 elevation_data.append(elevation)
-
             elif '<time' in line: # get trackpoints timestamp
                 tmp = re.findall('\d+.*Z', line)
 
@@ -70,7 +82,7 @@ def main(): # main script
     elevation_data = np.array(elevation_data) # [m]
     timestamp_data = np.array(timestamp_data) # [sec]
 
-    # calculate trackpoints distance, slope and speed
+    # calculate trackpoints distance, slope, speed and power
     distance_data = np.zeros(timestamp_data.shape) # [m]
     slope_data = np.zeros(timestamp_data.shape) # [%]
 
@@ -90,17 +102,15 @@ def main(): # main script
 
         delta_elevation = elevation_data[i]-elevation_data[i-1]
 
-        slope_data[i] = 100*(delta_elevation/distance_data[i])
+        slope_data[i] = delta_elevation/distance_data[i]
 
-        distance_data[i] = np.sqrt(np.power(distance_data[i], 2)+np.power(delta_elevation, 2)) # recalculate distance to take into account the slope
+        distance_data[i] = np.sqrt(np.power(distance_data[i], 2)+np.power(delta_elevation, 2)) # recalculate distance to take slope into account
 
-    speed_data = np.zeros(timestamp_data.shape) # [mph]
+    speed_data = np.zeros(timestamp_data.shape) # [m/s]
 
     for i in np.arange(1, timestamp_data.shape[0]):
         if timestamp_data[i] != timestamp_data[i-1]:
-            speed = distance_data[i]/(timestamp_data[i]-timestamp_data[i-1])
-
-            speed_data[i] = speed*2.236936 # convert m/s to mph
+            speed_data[i] = distance_data[i]/(timestamp_data[i]-timestamp_data[i-1])
         else:
             speed_data[i] = 0
 
@@ -108,10 +118,22 @@ def main(): # main script
     slope_data = medfilt(slope_data, 5)
     speed_data = medfilt(speed_data, 5)
 
+    power_data = np.zeros(timestamp_data.shape) # [watt]
+
+    for i in np.arange(1, timestamp_data.shape[0]):
+        speed = speed_data[i]
+        slope = slope_data[i]
+
+        power = (1/(1-bike_drivetrain_loss/100))*(g*(rider_weight+bike_weight)*(np.sin(np.arctan(slope))+bike_rr_coeff*np.cos(np.arctan(slope)))+(0.5*rider_bike_drag_coeff*rider_bike_frontal_area*air_density*np.power(speed, 2)))*speed
+
+        if power > 0:
+            power_data[i] = power
+
     # normalize data for visualization
     elevation_data_norm = (elevation_data-elevation_data.min())/(elevation_data.max()-elevation_data.min())
     slope_data_norm = (slope_data-slope_data.min())/(slope_data.max()-slope_data.min())
     speed_data_norm = (speed_data-speed_data.min())/(speed_data.max()-speed_data.min())
+    power_data_norm = (power_data-power_data.min())/(power_data.max()-power_data.min())
 
     # create GeoJSON feature collection
     features = []
@@ -123,14 +145,16 @@ def main(): # main script
         if vis_data == 'none':
             color = '#FC4C02'
         elif vis_data == 'elevation':
-            color = to_hex(cmap(elevation_data_norm[i]))
+            color = rgb2hex(cmap(elevation_data_norm[i]))
         elif vis_data == 'slope':
-            color = to_hex(cmap(slope_data_norm[i]))
+            color = rgb2hex(cmap(slope_data_norm[i]))
         elif vis_data == 'speed':
-            color = to_hex(cmap(speed_data_norm[i]))
+            color = rgb2hex(cmap(speed_data_norm[i]))
+        elif vis_data == 'power':
+            color = rgb2hex(cmap(power_data_norm[i]))
 
         if vis_medium == 'raw':
-            feature = geojson.Feature(geometry = line, properties = {"elevation (m)": str(elevation_data[i]), "slope (%)": str(slope_data[i]), "speed (mph)": str(speed_data[i])}) # export all data
+            feature = geojson.Feature(geometry = line, properties = {"elevation (m)": str(elevation_data[i]), "slope (%)": str(slope_data[i]*100), "speed (mph)": str(speed_data[i]*2.236936)}) # export all data
         elif vis_medium == 'geojsonio':
             feature = geojson.Feature(geometry = line, properties = {"stroke": color, "stroke-width": 5}) # export color for geojson.io
         elif vis_medium == 'umap':
