beb50ccd8fd8aa5dadbc2594f4cd3d23fadf3efb
Remi Salmon <salmon.remi@gmail.com>
2019-02-13 14:52:14 -0700

    Add Ramer–Douglas–Peucker algorithm to reduce GeoJSON size when --visualize is used

--- strava_geojson.py
+++ strava_geojson.py
@@ -36,10 +36,64 @@ def rgb2hex(c):
     hexc = '#%02x%02x%02x'%(int(c[0]*255), int(c[1]*255), int(c[2]*255))
     return(hexc)
 
-def gpx2geojson(gpx_file, geojson_file, param, use_SI):
+def distLatLon(p1, p2): # Haversine formula distance between p1 and p2 [lat,lon] (in deg)
+    lat1 = np.radians(p1[0])
+    lat2 = np.radians(p2[0])
+    lon1 = np.radians(p1[1])
+    lon2 = np.radians(p2[1])
+
+    delta_lat = lat2-lat1
+    delta_lon = lon2-lon1
+
+    a = np.power(np.sin(delta_lat/2.0), 2)+np.cos(lat1)*np.cos(lat2)*np.power(np.sin(delta_lon/2.0), 2)
+    c = 2.0*np.arctan2(np.sqrt(a), np.sqrt(1.0-a))
+
+    dist = 6371e3*c
+
+    return(dist)
+
+def distLatLonPoint2Line(p0, p1, p2): # distance from p0 to line defined by p1 and p2 [lat,lon] (in deg)
+    # Mercator projection
+    P0 = np.array([np.radians(p0[1]), np.arcsinh(np.tan(np.radians(p0[0])))])*6371e3
+    P1 = np.array([np.radians(p1[1]), np.arcsinh(np.tan(np.radians(p1[0])))])*6371e3
+    P2 = np.array([np.radians(p2[1]), np.arcsinh(np.tan(np.radians(p2[0])))])*6371e3
+
+    # distance from point to line
+    dist = abs((P2[1]-P1[1])*P0[0]-(P2[0]-P1[0])*P0[1]+P2[0]*P1[1]-P2[1]*P1[0])/np.sqrt(np.power(P2[1]-P1[1], 2)+np.power(P2[0]-P1[0], 2)) # (from https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line#Line_defined_by_two_points)
+
+    return(dist)
+
+def RDP(data, epsilon): # Ramer–Douglas–Peucker algorithm
+    # (from https://en.wikipedia.org/wiki/Ramer%E2%80%93Douglas%E2%80%93Peucker_algorithm)
+    dist_max = 0
+    index = 0
+
+    for i in np.arange(1, data.shape[0]):
+        dist = distLatLonPoint2Line(data[i, :2], data[0, :2], data[-1, :2]) # needs a 2D projection, does not work with cross-track distance
+
+        if dist > dist_max:
+            index = i
+            dist_max = dist
+
+    if dist_max > epsilon:
+        tmp1 = RDP(data[:index+1, :], epsilon)
+        tmp2 = RDP(data[index:, :], epsilon)
+
+        data_new = np.vstack((tmp1[:-1], tmp2))
+    else:
+        data_new = np.vstack((data[0, :], data[-1, :]))
+
+    return(data_new)
+
+def gpx2geojson(gpx_file, geojson_file, param = [0, 0], use_SI = False, use_RDP = False):
     # parameters
     rider_weight = param[0] # kg
     bike_weight = param[1] # kg
+
+    if rider_weight*bike_weight == 0:
+        get_power_data = False
+    else:
+        get_power_data = True
 
     # constants
     rider_bike_frontal_area = 0.632 # m^2 (from https://www.cyclingpowerlab.com/cyclingaerodynamics.aspx)
@@ -48,11 +102,6 @@ def gpx2geojson(gpx_file, geojson_file, param, use_SI):
     bike_rr_coeff = 0.006 # unitless (from https://en.wikipedia.org/wiki/Bicycle_performance#Rolling_resistance)
     air_density = 1.225 # kg/m^3
     g = 9.80665 # m/s^2
-
-    if rider_weight*bike_weight == 0:
-        get_power_data = False
-    else:
-        get_power_data = True
 
     # initialize lists
     lat_lon_data = []
@@ -83,18 +132,7 @@ def gpx2geojson(gpx_file, geojson_file, param, use_SI):
     speed_data = np.zeros(timestamp_data.shape) # [m/s]
 
     for i in np.arange(1, timestamp_data.shape[0]):
-        lat1 = np.radians(lat_lon_data[i-1, 0])
-        lat2 = np.radians(lat_lon_data[i, 0])
-        lon1 = np.radians(lat_lon_data[i-1, 1])
-        lon2 = np.radians(lat_lon_data[i, 1])
-
-        delta_lat = lat2-lat1
-        delta_lon = lon2-lon1
-
-        a = np.power(np.sin(delta_lat/2.0), 2)+np.cos(lat1)*np.cos(lat2)*np.power(np.sin(delta_lon/2.0), 2)
-        c = 2.0*np.arctan2(np.sqrt(a), np.sqrt(1.0-a))
-
-        distance_data[i] = 6371e3*c # haversine formula
+        distance_data[i] = distLatLon(lat_lon_data[i-1, :], lat_lon_data[i, :])
 
         delta_elevation = elevation_data[i]-elevation_data[i-1]
 
@@ -125,6 +163,25 @@ def gpx2geojson(gpx_file, geojson_file, param, use_SI):
             if power > 0:
                 power_data[i] = power
 
+    # use Ramer–Douglas–Peucker algorithm to reduce the number of trackpoints
+    if use_RDP:
+        epsilon = 1 # m
+
+        tmp = np.hstack((lat_lon_data, np.arange(0, lat_lon_data.shape[0]).reshape((-1, 1)))) # hack
+
+        tmp_new = RDP(tmp, epsilon) # remove trackpoints less than epsilon meters away from the new track
+
+        index = tmp_new[:, 2].astype(int) # hack
+
+        lat_lon_data = lat_lon_data[index, :]
+        elevation_data = elevation_data[index]
+        timestamp_data = timestamp_data[index]
+        distance_data = distance_data[index]
+        slope_data = slope_data[index]
+        speed_data = speed_data[index]
+        if get_power_data:
+            power_data = power_data[index]
+
     # convert units
     if use_SI:
         speed_data = speed_data*3.6 # m/s to km/h
@@ -154,7 +211,7 @@ def gpx2geojson(gpx_file, geojson_file, param, use_SI):
 
     return
 
-def geojson2folium(geojson_file, use_SI):
+def geojson2folium(geojson_file, use_SI = False):
     # read GeoJSON file
     with open(geojson_file, 'r') as file:
         geojson_data = geojson.load(file)
@@ -178,7 +235,7 @@ def geojson2folium(geojson_file, use_SI):
 
     cmap = cm.get_cmap('jet') # matplotlib colormap
 
-    # hack to reduce GeoJSON data sent to Folium map as layer
+    # [hack] create new GeoJson objects to reduce GeoJSON data sent to Folium map as layer
     f_track = lambda x: {'color': '#FC4C02', 'weight': 5} # show some color...
 
     features = []
@@ -276,9 +333,9 @@ def main(args): # main script
     gpx_filename = args.gpxfile # str
     geojson_filename = args.geojsonfile # str
     visualize = args.visualize # bool
-    use_SI = args.SI # bool
-    rider_weight = args.riderweight if use_SI else args.riderweight*0.45359237 # float (kg)
-    bike_weight = args.bikeweight if use_SI else args.bikeweight*0.45359237 # float (kg)
+    SI = args.SI # bool
+    rider_weight = args.riderweight if SI else args.riderweight*0.45359237 # float (kg)
+    bike_weight = args.bikeweight if SI else args.bikeweight*0.45359237 # float (kg)
 
     if not gpx_filename[-4:] == '.gpx':
         print('ERROR: --input is not a GPX file')
@@ -302,11 +359,11 @@ def main(args): # main script
     geojson_file = geojson_filename if geojson_filename else gpx_file[:-4]+'.geojson' # use GPX filename if not specified
 
     # write GeoJSON file
-    gpx2geojson(gpx_file, geojson_file, [rider_weight, bike_weight], use_SI)
+    gpx2geojson(gpx_file, geojson_file, param = [rider_weight, bike_weight], use_SI = SI, use_RDP = visualize)
 
     # visualize GeoJSON file with Folium
     if visualize:
-        geojson2folium(geojson_file, use_SI)
+        geojson2folium(geojson_file, use_SI = SI)
 
 if __name__ == '__main__':
     # command line parameters
